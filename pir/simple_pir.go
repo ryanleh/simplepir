6ce03985aff5c0274da795903212af90ac67d46d
ahenzinger <ahenzinger@gmail.com>
2023-02-16 21:43:29 -0500

    bug fix

--- pir/simple_pir.go
+++ pir/simple_pir.go
@@ -81,8 +81,8 @@ func (s *Server) DBInfo() *DBInfo {
 	return s.db.Info
 }
 
-func (s *Server) Get(i, j uint64) uint64 {
-	return s.db.Data.Get(i, j)
+func (s *Server) Get(i uint64) uint64 {
+	return s.db.GetElem(i)
 }
 
 func (s *Server) GobEncode() ([]byte, error) {
