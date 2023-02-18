569ac459f920c2d681fd9cd59ee6ec6e1454bd40
Henry Corrigan-Gibbs <henrycg@csail.mit.edu>
2023-02-18 18:30:19 -0500

    PIR tests for 32- and 64-bit

--- pir/pir_test.go
+++ pir/pir_test.go
@@ -23,43 +23,47 @@ func testServerEncode[T matrix.Elem](t *testing.T, N, d uint64) {
 	enc := gob.NewEncoder(&b)
 	err := enc.Encode(server)
 	if err != nil {
-		panic("Encoding failed")
+		t.Fatal("Encoding failed")
 	}
 
 	dec := gob.NewDecoder(&b)
 	var server2 Server[T]
 	err = dec.Decode(&server2)
 	if err != nil {
-		panic("Decoding failed")
+		t.Fatal("Decoding failed")
 	}
 
 	if *server2.params != *server.params {
-		panic("Parameter mismatch")
+		t.Fatal("Parameter mismatch")
 	}
 	if !server2.matrixA.Equals(server.matrixA) {
-		panic("A matrix mismatch")
+		t.Fatal("A matrix mismatch")
 	}
 	if !server2.hint.Equals(server.hint) {
-		panic("Hint mismatch")
+		t.Fatal("Hint mismatch")
 	}
 
 	if server2.db.Info.Num != server.db.Info.Num {
-		panic("DB info mismatch")
+		t.Fatal("DB info mismatch")
 	}
 	if server2.db.Info.Params.N != server.db.Info.Params.N {
-		panic("DB info mismatch")
+		t.Fatal("DB info mismatch")
 	}
 	if !server2.db.Data.Equals(server.db.Data) {
-		panic("DB mismatch")
+		t.Fatal("DB mismatch")
 	}
 }
 
-func TestServerEncode(t *testing.T) {
+func TestServerEncode32(t *testing.T) {
 	testServerEncode[matrix.Elem32](t, uint64(1<<20), uint64(8))
 }
 
+func TestServerEncode64(t *testing.T) {
+	testServerEncode[matrix.Elem64](t, uint64(1<<20), uint64(8))
+}
+
 // Run full PIR scheme (offline + online phases).
-func runPIR[T matrix.Elem](client *Client[T], server *Server[T], db *Database[T], i uint64) {
+func runPIR[T matrix.Elem](t *testing.T, client *Client[T], server *Server[T], db *Database[T], i uint64) {
 	secret, query := client.Query(i)
 	answer := server.Answer(query)
 
@@ -68,11 +72,11 @@ func runPIR[T matrix.Elem](client *Client[T], server *Server[T], db *Database[T]
 	if db.GetElem(i) != val {
 		fmt.Printf("(querying index %d -- row should be >= %d): Got %d instead of %d\n",
 			i, db.Data.Rows()/4, val, db.GetElem(i))
-		panic("Reconstruct failed!")
+		t.Fatal()
 	}
 }
 
-func runPIRmany[T matrix.Elem](client *Client[T], server *Server[T], db *Database[T], i uint64) {
+func runPIRmany[T matrix.Elem](t *testing.T, client *Client[T], server *Server[T], db *Database[T], i uint64) {
 	secret, query := client.Query(i)
 	answer := server.Answer(query)
 
@@ -84,12 +88,12 @@ func runPIRmany[T matrix.Elem](client *Client[T], server *Server[T], db *Databas
 		if db.GetElem(index) != vals[row] {
 			fmt.Printf("Querying index %d: Got %d instead of %d\n",
 				index, vals[row], db.GetElem(index))
-			panic("Reconstruct failed!")
+			t.Fatal("Reconstruct failed!")
 		}
 	}
 }
 
-func runLHE[T matrix.Elem](client *Client[T], server *Server[T], db *Database[T], arr []uint64) {
+func runLHE[T matrix.Elem](t *testing.T, client *Client[T], server *Server[T], db *Database[T], arr []uint64) {
 	secret, query := client.QueryLHE(arr)
 	answer := server.Answer(query)
 
@@ -108,7 +112,7 @@ func runLHE[T matrix.Elem](client *Client[T], server *Server[T], db *Database[T]
 		if should_be != vals[i] {
 			fmt.Printf("Row %d: Got %d instead of %d (mod %d)\n",
 				i, vals[i], should_be, mod)
-			panic("Reconstruct failed!")
+			t.Fatal("Reconstruct failed!")
 		}
 	}
 }
@@ -118,7 +122,7 @@ func testDBInit[T matrix.Elem](t *testing.T, N uint64, d uint64, vals []uint64) 
 
 	for i := uint64(0); i < N; i++ {
 		if db.GetElem(i) != (i + 1) {
-			t.FailNow()
+			t.Fatal("Reconstruct failed!")
 		}
 	}
 
@@ -126,165 +130,253 @@ func testDBInit[T matrix.Elem](t *testing.T, N uint64, d uint64, vals []uint64) 
 }
 
 // Test that DB packing methods are correct, when each database entry is ~ 1 Z_p elem.
-func TestDBMediumEntries(t *testing.T) {
+func testDBMediumEntries[T matrix.Elem](t *testing.T) {
 	vals := []uint64{1, 2, 3, 4}
-	db := testDBInit[matrix.Elem32](t, uint64(4), uint64(9), vals)
+	db := testDBInit[T](t, uint64(4), uint64(9), vals)
 
 	if db.Info.Packing != 1 || db.Info.Ne != 1 {
-		t.FailNow()
+		t.Fail()
 	}
+}
+
+func TestDBMediumEntries32(t *testing.T) {
+  testDBMediumEntries[matrix.Elem32](t)
+}
+
+func TestDBMediumEntries64(t *testing.T) {
+  testDBMediumEntries[matrix.Elem64](t)
 }
 
 // Test that DB packing methods are correct, when multiple database entries fit in 1 Z_p elem.
-func TestDBSmallEntries(t *testing.T) {
+func testDBSmallEntries[T matrix.Elem](t *testing.T) {
 	vals := []uint64{1, 2, 3, 4}
-	db := testDBInit[matrix.Elem32](t, uint64(4), uint64(3), vals)
+	db := testDBInit[T](t, uint64(4), uint64(3), vals)
 
 	if db.Info.Packing <= 1 || db.Info.Ne != 1 {
-		t.FailNow()
+		t.Fail()
 	}
+}
+
+func TestDBSmallEntries32(t *testing.T) {
+  testDBSmallEntries[matrix.Elem32](t)
+}
+
+func TestDBSmallEntries64(t *testing.T) {
+  testDBSmallEntries[matrix.Elem64](t)
 }
 
 // Test that DB packing methods are correct, when each database entry requires multiple Z_p elems.
-func TestDBLargeEntries(t *testing.T) {
+func testDBLargeEntries[T matrix.Elem](t *testing.T) {
 	vals := []uint64{1, 2, 3, 4}
-	db := testDBInit[matrix.Elem32](t, uint64(4), uint64(12), vals)
+	db := testDBInit[T](t, uint64(4), uint64(12), vals)
 
 	if db.Info.Packing != 0 || db.Info.Ne <= 1 {
-		panic("Should not happen.")
+		t.Fatal()
 	}
 }
 
-func testSimplePir(t *testing.T, N uint64, d uint64, index uint64) {
+func TestDBLargeEntries32(t *testing.T) {
+  testDBLargeEntries[matrix.Elem64](t)
+}
+
+func TestDBLargeEntries64(t *testing.T) {
+  testDBLargeEntries[matrix.Elem64](t)
+}
+
+func testSimplePir[T matrix.Elem](t *testing.T, N uint64, d uint64, index uint64) {
 	prg := rand.NewRandomBufPRG()
-	db := NewDatabaseRandom[matrix.Elem32] (prg, LOGQ, N, d)
+	db := NewDatabaseRandom[T](prg, LOGQ, N, d)
 
 	server := NewServer(db)
 	client := NewClient(server.Hint(), server.MatrixA(), db.Info)
 
-	runPIR(client, server, db, index)
+	runPIR(t, client, server, db, index)
 }
 
-func testSimplePirMany(t *testing.T, N uint64, d uint64, index uint64) {
+func testSimplePirMany[T matrix.Elem](t *testing.T, N uint64, d uint64, index uint64) {
 	prg := rand.NewRandomBufPRG()
-	db := NewDatabaseRandom[matrix.Elem32] (prg, LOGQ, N, d)
+	db := NewDatabaseRandom[T](prg, LOGQ, N, d)
 
 	server := NewServer(db)
 	client := NewClient(server.Hint(), server.MatrixA(), db.Info)
 
-	runPIRmany(client, server, db, index)
+	runPIRmany(t, client, server, db, index)
 }
 
-func testLHE(t *testing.T, N uint64, d uint64) {
+func testLHE[T matrix.Elem](t *testing.T, N uint64, d uint64) {
 	prg := rand.NewRandomBufPRG()
 	params := lwe.NewParamsFixedP(LOGQ, N, 1024)
-	db := NewDatabaseRandomFixedParams[matrix.Elem32](prg, N, d, params)
+	db := NewDatabaseRandomFixedParams[T](prg, N, d, params)
 
 	server := NewServer(db)
 	client := NewClient(server.Hint(), server.MatrixA(), db.Info)
 
 	arr := RandArray(db.Info.M, db.Info.P())
-	runLHE(client, server, db, arr)
+	runLHE(t, client, server, db, arr)
 }
 
-func testSimplePirCompressed(t *testing.T, N uint64, d uint64, index uint64) {
+func testSimplePirCompressed[T matrix.Elem](t *testing.T, N uint64, d uint64, index uint64) {
 	prg := rand.NewRandomBufPRG()
-	db := NewDatabaseRandom[matrix.Elem32](prg, LOGQ, N, d)
+	db := NewDatabaseRandom[T](prg, LOGQ, N, d)
 
 	seed := rand.RandomPRGKey()
 	server := NewServerSeed(db, seed)
 	client := NewClient(server.Hint(), server.MatrixA(), db.Info)
 
-	runPIR(client, server, db, index)
+	runPIR(t, client, server, db, index)
 }
 
-func testSimplePirCompressedMany(t *testing.T, N uint64, d uint64, index uint64) {
+func testSimplePirCompressedMany[T matrix.Elem](t *testing.T, N uint64, d uint64, index uint64) {
 	prg := rand.NewRandomBufPRG()
-	db := NewDatabaseRandom[matrix.Elem32](prg, LOGQ, N, d)
+	db := NewDatabaseRandom[T](prg, LOGQ, N, d)
 
 	seed := rand.RandomPRGKey()
 	server := NewServerSeed(db, seed)
 	client := NewClient(server.Hint(), server.MatrixA(), db.Info)
 
-	runPIRmany(client, server, db, index)
+	runPIRmany(t, client, server, db, index)
 }
 
-func testLHECompressed(t *testing.T, N uint64, d uint64) {
+func testLHECompressed[T matrix.Elem](t *testing.T, N uint64, d uint64) {
 	prg := rand.NewRandomBufPRG()
 	params := lwe.NewParamsFixedP(LOGQ, N, 1024)
-	db := NewDatabaseRandomFixedParams[matrix.Elem32](prg, N, d, params)
+	db := NewDatabaseRandomFixedParams[T](prg, N, d, params)
 
 	seed := rand.RandomPRGKey()
 	server := NewServerSeed(db, seed)
 	client := NewClient(server.Hint(), server.MatrixA(), db.Info)
 
 	arr := RandArray(db.Info.M, db.Info.P())
-	runLHE(client, server, db, arr)
+	runLHE(t, client, server, db, arr)
 }
 
 // Test SimplePIR correctness on DB with short entries.
-func TestSimplePir(t *testing.T) {
-	testSimplePir(t, uint64(1<<20), uint64(8), 262144)
+func TestSimplePir32(t *testing.T) {
+	testSimplePir[matrix.Elem32](t, uint64(1<<20), uint64(8), 262144)
 }
 
-func TestSimplePirMany(t *testing.T) {
-	testSimplePirMany(t, uint64(1<<20), uint64(8), 262144)
+func TestSimplePir64(t *testing.T) {
+	testSimplePir[matrix.Elem64](t, uint64(1<<20), uint64(8), 262144)
 }
 
-func TestLHE(t *testing.T) {
-	testLHE(t, uint64(1<<20), uint64(9))
+func TestSimplePirMany32(t *testing.T) {
+	testSimplePirMany[matrix.Elem32](t, uint64(1<<20), uint64(8), 262144)
 }
 
-func TestLHE2(t *testing.T) {
-	testLHE(t, uint64(1<<20), uint64(8))
+func TestSimplePirMany64(t *testing.T) {
+	testSimplePirMany[matrix.Elem64](t, uint64(1<<20), uint64(8), 262144)
 }
 
-func TestLHE3(t *testing.T) {
-	testLHE(t, uint64(1<<20), uint64(6))
+func TestLHE32(t *testing.T) {
+	testLHE[matrix.Elem32](t, uint64(1<<20), uint64(9))
 }
 
-func TestSimplePirCompressed(t *testing.T) {
-	testSimplePirCompressed(t, uint64(1<<20), uint64(8), 262144)
+func TestLHE64(t *testing.T) {
+	testLHE[matrix.Elem64](t, uint64(1<<20), uint64(9))
 }
 
-func TestSimplePirCompressedMany(t *testing.T) {
-	testSimplePirCompressedMany(t, uint64(1<<20), uint64(8), 262144)
+func TestLHE32_2(t *testing.T) {
+	testLHE[matrix.Elem32](t, uint64(1<<20), uint64(8))
 }
 
-func TestLHECompressed(t *testing.T) {
-	testLHECompressed(t, uint64(1<<20), uint64(9))
+func TestLHE64_2(t *testing.T) {
+	testLHE[matrix.Elem64](t, uint64(1<<20), uint64(8))
+}
+
+func TestLHE32_3(t *testing.T) {
+	testLHE[matrix.Elem32](t, uint64(1<<20), uint64(6))
+}
+
+func TestLHE64_3(t *testing.T) {
+	testLHE[matrix.Elem64](t, uint64(1<<20), uint64(6))
+}
+
+func TestSimplePirCompressed32(t *testing.T) {
+	testSimplePirCompressed[matrix.Elem32](t, uint64(1<<20), uint64(8), 262144)
+}
+
+func TestSimplePirCompressed64(t *testing.T) {
+	testSimplePirCompressed[matrix.Elem64](t, uint64(1<<20), uint64(8), 262144)
+}
+
+func TestSimplePirCompressedMany32(t *testing.T) {
+	testSimplePirCompressedMany[matrix.Elem32](t, uint64(1<<20), uint64(8), 262144)
+}
+
+func TestSimplePirCompressedMany64(t *testing.T) {
+	testSimplePirCompressedMany[matrix.Elem64](t, uint64(1<<20), uint64(8), 262144)
+}
+
+func TestLHECompressed32(t *testing.T) {
+	testLHECompressed[matrix.Elem32](t, uint64(1<<20), uint64(9))
+}
+
+func TestLHECompressed64(t *testing.T) {
+	testLHECompressed[matrix.Elem64](t, uint64(1<<20), uint64(9))
 }
 
 // Test SimplePIR correctness on DB with long entries
-func TestSimplePirLongRow(t *testing.T) {
-	testSimplePir(t, uint64(1<<20), uint64(32), 1)
+func TestSimplePirLongRow32(t *testing.T) {
+	testSimplePir[matrix.Elem32](t, uint64(1<<20), uint64(32), 1)
 }
 
-func TestSimplePirLongRowCompressed(t *testing.T) {
-	testSimplePirCompressed(t, uint64(1<<20), uint64(32), 1)
+func TestSimplePirLongRow64(t *testing.T) {
+	testSimplePir[matrix.Elem64](t, uint64(1<<20), uint64(64), 1)
+}
+
+func TestSimplePirLongRowCompressed32(t *testing.T) {
+	testSimplePirCompressed[matrix.Elem32](t, uint64(1<<20), uint64(32), 1)
+}
+
+func TestSimplePirLongRowCompressed64(t *testing.T) {
+	testSimplePirCompressed[matrix.Elem64](t, uint64(1<<20), uint64(64), 1)
 }
 
 // Test SimplePIR correctness on big DB
-func TestSimplePirBigDB(t *testing.T) {
-	testSimplePir(t, uint64(1<<25), uint64(7), 0)
+func TestSimplePirBigDB32(t *testing.T) {
+	testSimplePir[matrix.Elem32](t, uint64(1<<25), uint64(7), 0)
 }
 
-func TestSimplePirBigDBmany(t *testing.T) {
-	testSimplePirMany(t, uint64(1<<25), uint64(7), 0)
+func TestSimplePirBigDB64(t *testing.T) {
+	testSimplePir[matrix.Elem64](t, uint64(1<<25), uint64(7), 0)
 }
 
-func TestLHEBigDB(t *testing.T) {
-	testLHE(t, uint64(1<<25), uint64(9))
+func TestSimplePirBigDBmany32(t *testing.T) {
+	testSimplePirMany[matrix.Elem32](t, uint64(1<<25), uint64(7), 0)
 }
 
-func TestSimplePirBigDBCompressed(t *testing.T) {
-	testSimplePirCompressed(t, uint64(1<<25), uint64(7), 0)
+func TestSimplePirBigDBmany64(t *testing.T) {
+	testSimplePirMany[matrix.Elem64](t, uint64(1<<25), uint64(7), 0)
 }
 
-func TestSimplePirBigDBCompressedMany(t *testing.T) {
-	testSimplePirCompressedMany(t, uint64(1<<25), uint64(7), 0)
+func TestLHEBigDB32(t *testing.T) {
+	testLHE[matrix.Elem32](t, uint64(1<<25), uint64(9))
 }
 
-func TestLHEBigDBCompressed(t *testing.T) {
-	testLHECompressed(t, uint64(1<<25), uint64(9))
+func TestLHEBigDB64(t *testing.T) {
+	testLHE[matrix.Elem64](t, uint64(1<<25), uint64(9))
+}
+
+func TestSimplePirBigDBCompressed32(t *testing.T) {
+	testSimplePirCompressed[matrix.Elem32](t, uint64(1<<25), uint64(7), 0)
+}
+
+func TestSimplePirBigDBCompressed64(t *testing.T) {
+	testSimplePirCompressed[matrix.Elem64](t, uint64(1<<25), uint64(7), 0)
+}
+
+func TestSimplePirBigDBCompressedMany32(t *testing.T) {
+	testSimplePirCompressedMany[matrix.Elem32](t, uint64(1<<25), uint64(7), 0)
+}
+
+func TestSimplePirBigDBCompressedMany64(t *testing.T) {
+	testSimplePirCompressedMany[matrix.Elem64](t, uint64(1<<25), uint64(7), 0)
+}
+
+func TestLHEBigDBCompressed32(t *testing.T) {
+	testLHECompressed[matrix.Elem32](t, uint64(1<<25), uint64(9))
+}
+
+func TestLHEBigDBCompressed64(t *testing.T) {
+	testLHECompressed[matrix.Elem64](t, uint64(1<<25), uint64(9))
 }
